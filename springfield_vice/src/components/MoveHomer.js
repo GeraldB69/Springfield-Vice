23ecf572257e9889f34da3ef0e81e419b2618f71
irvintarlao <irvin.tarlao@gmail.com>
2019-10-21 13:57:03 +0200

    déplacement homer ok

--- springfield_vice/src/components/MoveHomer.js
+++ springfield_vice/src/components/MoveHomer.js
@@ -1,27 +1,10 @@
 import React from 'react';
+import config from "./configSpringfieldVice.json";
 
 class MoveHomer extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            positionX: 100,
-            positionY: 100,
-        }
-
-
-    }
-
-    move = (stepX,stepY) => {
-        const { positionX, positionY} = this.state;
-        this.setState({ 
-            positionX: positionX+stepX, 
-            positionY: positionY+stepY,
-        });
-        this.timeOut = setTimeout(() => this.move(stepX, stepY), 1); //1ms
-    }
-
-    stopMove = () => {
-        clearTimeout(this.timeOut);
+        this.state = {}
     }
 
     render = () => {
@@ -37,56 +20,56 @@ class MoveHomer extends React.Component {
         return (
             <div>
                 
-                <button onPointerDown={() => this.move(0,-5)} onPointerUp={this.stopMove} style={
+                <button onPointerDown={() => this.props.move(config.homerSpeed.x,-config.homerSpeed.y)} onPointerUp={this.props.stopMove} style={
                         {
                             ...buttonStyleMain,
                             top: "70vh",
                             left: "10vw",
                         }}>up
                 </button>
-                <button onPointerDown={() => this.move(5,0)} onPointerUp={this.stopMove} style={
+                <button onPointerDown={() => this.props.move(config.homerSpeed.x,0)} onPointerUp={this.props.stopMove} style={
                         {
                             ...buttonStyleMain,
                             top: "75vh",
                             left: "15vw",
                         }}>right
                 </button>
-                <button onPointerDown={() => this.move(0,5)} onPointerUp={this.stopMove} style={
+                <button onPointerDown={() => this.props.move(0,config.homerSpeed.y)} onPointerUp={this.props.stopMove} style={
                         {
                             ...buttonStyleMain,
                             top: "80vh",
                             left: "10vw",
                         }}>down
                 </button>
-                <button onPointerDown={() => this.move(-5,0)} onPointerUp={this.stopMove} style={
+                <button onPointerDown={() => this.props.move(-config.homerSpeed.x,0)} onPointerUp={this.props.stopMove} style={
                         {
                             ...buttonStyleMain,
                             top: "75vh",
                             left: "5vw",
                         }}>left
                 </button>
-                <button onPointerDown={() => this.move(5,5)} onPointerUp={this.stopMove} style={
+                <button onPointerDown={() => this.props.move(config.homerSpeed.x,config.homerSpeed.y)} onPointerUp={this.props.stopMove} style={
                         {
                             ...buttonStyleMain,
                             top: "85vh",
                             left: "15vw",
                         }}>right and down
                 </button>
-                <button onPointerDown={() => this.move(5,-5)} onPointerUp={this.stopMove} style={
+                <button onPointerDown={() => this.props.move(config.homerSpeed.x,-config.homerSpeed.y)} onPointerUp={this.props.stopMove} style={
                         {
                             ...buttonStyleMain,
                             top: "65vh",
                             left: "15vw",
                         }}>right and up
                 </button>
-                <button onPointerDown={() => this.move(-5,5)} onPointerUp={this.stopMove} style={
+                <button onPointerDown={() => this.props.move(-config.homerSpeed.x,config.homerSpeed.y)} onPointerUp={this.props.stopMove} style={
                         {
                             ...buttonStyleMain,
                             top: "85vh",
                             left: "5vw",
                         }}>left and down
                 </button>
-                <button onPointerDown={() => this.move(-5,-5)} onPointerUp={this.stopMove} style={
+                <button onPointerDown={() => this.props.move(-config.homerSpeed.x,-config.homerSpeed.y)} onPointerUp={this.props.stopMove} style={
                         {
                             ...buttonStyleMain,
                             top: "65vh",
