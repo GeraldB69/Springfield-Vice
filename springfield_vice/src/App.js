a7dc452d207e338b6b952c0cddc54593b9820954
Anicia Tarlao <tarlaoanicia@gmail.com>
2019-10-21 11:30:10 +0200

    Timer ok

--- springfield_vice/src/App.js
+++ springfield_vice/src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
 import "./App.css";
-import Homer from "./components/Homer";
-import Timer from "./components/Timer";
 import Game from "./pages/Game";
 
 function App() {
--- springfield_vice/src/pages/Game.js
+++ springfield_vice/src/pages/Game.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Homer from "../components/Homer";
 import PadTouch from "../components/PadTouch";
 import config from "../components/configSpringfieldVice.json";
+import Timer from "../components/Timer";
 
 class Game extends Component {
 	constructor(props) {
@@ -39,6 +40,7 @@ class Game extends Component {
 				{this.testLimitsOfMap()}
 
 				<Homer positionX={this.state.positionX} positionY={this.state.positionY} />
+				<Timer />
 			</div>
 		);
 	}
