54c885dd3004f8ff17fc613bf8e79f2cf6d5b5fe
Anicia Tarlao <tarlaoanicia@gmail.com>
2019-10-22 20:28:35 +0200

    Reset npm

---
+++ springfield-vice/src/components/Homer.js
@@ -0,0 +1,74 @@
+import React, { Component } from "react";
+import "./homer.css";
+import config from "./configSpringfieldVice.json";
+
+import homer from "./img/3.gif";
+import donut from "./img/donut.png";
+import ripchain from "./img/ripchain.png";
+
+class Homer extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			ripchain: false,
+			donut: true,
+			throwing: false
+		};
+	}
+
+	throwingDonut = () => {
+		this.setState({ throwing: !this.state.throwing });
+		//this.setState({ donut: false }); // A decommenter por faire disparaitre le donut lancé
+	};
+
+	render() {
+		const scaledPosY = this.props.positionY * config.homerSize.scale;
+
+		const positionDonutX = this.state.throwing
+			? parseInt(this.props.positionX) + 845
+			: parseInt(this.props.positionX) + 45;
+
+		const displayDonut = this.state.donut ? "block" : "none";
+		const displayRipchain = this.state.ripchain ? "block" : "none";
+		console.log("coordonnées : ", this.props.positionX, " - ", this.props.positionY);
+
+		const homerStyle = {
+			backgroundColor: "transparent",
+			padding: "0px",
+			margin: "0px",
+			height: config.homerSize.height,
+			position: "absolute",
+			left: `${this.props.positionX}px`,
+			top: `${this.props.positionY}px`,
+			transform: "scale(" + scaledPosY + ")"
+		};
+
+		const donutStyle = {
+			display: displayDonut,
+			width: "30px",
+			position: "absolute",
+			left: positionDonutX + "px",
+			top: 75 + parseInt(this.props.positionY) + "px",
+		};
+
+		const ripchainStyle = {
+			display: displayRipchain,
+			width: "60px",
+			position: "absolute",
+			left: positionDonutX + "px",
+			top: 75 + parseInt(this.props.positionY) + "px",
+			transform: "scale(" + scaledPosY + ")"
+		};
+
+		return (
+			<div>
+				<button onClick={this.throwingDonut}>DONUT</button>
+				<img src={homer} style={homerStyle} alt="homer" />
+				<img src={donut} style={donutStyle} className="vibrate-1" alt="donut" />
+				<img src={ripchain} style={ripchainStyle} alt="ripchain" />
+			</div>
+		);
+	}
+}
+
+export default Homer;
---
+++ springfield-vice/src/pages/Game.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import Homer from "../components/Homer";
+import config from "../components/configSpringfieldVice.json";
+import Timer from "../components/Timer";
+import MoveHomer from "../components/MoveHomer";
+import ModalPause from "../components/ModalPause";
+
+class Game extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			positionX: config.initialPosition.x,
+			positionY: config.initialPosition.y
+		};
+	}
+	
+
+	testLimitsOfMap = () => {
+		if (this.state.positionY < config.limits.topLimit) this.setState({ positionY: config.limits.topLimit });
+		else if (this.state.positionY > config.limits.bottomLimit)
+			this.setState({ positionY: config.limits.bottomLimit });
+		else if (this.state.positionX > config.limits.rightLimit)
+			this.setState({ positionX: config.limits.rightLimit });
+		else if (this.state.positionX < config.limits.leftLimit)
+			this.setState({ positionX: config.limits.leftLimit });
+	};
+
+	move = (stepX,stepY) => {
+        const { positionX, positionY} = this.state;
+        this.setState({ 
+            positionX: positionX+stepX, 
+            positionY: positionY+stepY,
+        });
+        this.timeOut = setTimeout(() => this.move(stepX, stepY), 20);
+	}
+	
+	stopMove = () => {
+        clearTimeout(this.timeOut);
+    }
+
+	render() {
+		return (
+			<div>
+				{this.testLimitsOfMap()}
+				<MoveHomer move={this.move} stopMove={this.stopMove}/>
+				<Homer positionX={this.state.positionX} positionY={this.state.positionY} />
+				<Timer />
+				<ModalPause show={this.state.show} handleClose={this.hideModal} >
+					<p>Modal</p>
+					<p>Data</p>
+				</ModalPause>
+			</div>
+		);
+	}
+}
+export default Game;
