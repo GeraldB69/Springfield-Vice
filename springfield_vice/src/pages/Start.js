395cdf12ffd7fd22780e29a75c7ec1820921460a
Gérald Barbosa <barbosa.gerald@gmail.com>
2019-10-24 13:38:06 +0200

    Pret pour les réglages

--- springfield_vice/src/pages/Start.js
+++ springfield_vice/src/pages/Start.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Game from './Game';
+//import Modal from '../components/Modal';
 import config from "../components/configSpringfieldVice.json";
 import "./start.css";
 
@@ -7,13 +8,18 @@ class Start extends Component {
   constructor(props) {
     super(props);
     this.state = ({
-      firstScreen: config.start.firstScreen
+      firstScreen: config.start.firstScreen,
+      modal: false
     })
   }
   result = "";
   startingGame = () => {
     this.setState({firstScreen: false});
     this.result = <Game />;
+  }
+  callOptions() {
+    // this.setState({modal: true});
+    // this.result = <Modal />;
   }
 
   render() {
@@ -23,7 +29,7 @@ class Start extends Component {
         <div className="start_div show" style={this.state.firstScreen === true ? {display: 'block'} :  {display: 'none'}} >
           <img className="left_img" src="logo512.png" alt="Springfield Vice" />
           <img className="home_screen" src='bart-ass.png' alt='Bart Simpsons' />
-          <button className="right_doh" onClick={() => this.startingGame()}>
+          <button className="right_doh" onClick={() => this.callOptions()}>
             <img src='homer-doh.png' alt='Homer d`oh' />
             <span className="text">Options</span>
           </button>
@@ -32,7 +38,7 @@ class Start extends Component {
             <span className="logo">&#9654;</span>
           </button>
         </div>
-        <div className={this.state.firstScreen === false ? 'game_div show' : 'game_div hide'} >
+        <div className={this.state.firstScreen === false || this.state.modal === true ? 'game_div show' : 'game_div hide'} >
           {this.result}
         </div>
       </div>
