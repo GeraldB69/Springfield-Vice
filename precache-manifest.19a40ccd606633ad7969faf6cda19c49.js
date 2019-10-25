88a5cc2b50297c46bba7f57113297be89e5281d1
Oliv <oallardjacquin@gmail.com>
2019-10-25 14:32:04 +0200

    Updates

---
+++ precache-manifest.19a40ccd606633ad7969faf6cda19c49.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b87256dea3836fee25a3e632c4dd6543",
+    "url": "/lyon-0919-react-p2-springfield-vice/index.html"
+  },
+  {
+    "revision": "d5665631be7538cfd6d6",
+    "url": "/lyon-0919-react-p2-springfield-vice/static/css/main.f7b03aaf.chunk.css"
+  },
+  {
+    "revision": "6b8b4d43704b959a644c",
+    "url": "/lyon-0919-react-p2-springfield-vice/static/js/2.eeb44845.chunk.js"
+  },
+  {
+    "revision": "d5665631be7538cfd6d6",
+    "url": "/lyon-0919-react-p2-springfield-vice/static/js/main.61aefab6.chunk.js"
+  },
+  {
+    "revision": "631aab3211d7b089809e",
+    "url": "/lyon-0919-react-p2-springfield-vice/static/js/runtime-main.5bb2186b.js"
+  },
+  {
+    "revision": "e80ccc64d05b3769c8e1cf91931ebe24",
+    "url": "/lyon-0919-react-p2-springfield-vice/static/media/3.e80ccc64.gif"
+  },
+  {
+    "revision": "cc871a94bbe4c6c5cbfdfdbb5fffef0f",
+    "url": "/lyon-0919-react-p2-springfield-vice/static/media/background_repeat.cc871a94.png"
+  },
+  {
+    "revision": "f51f0b3191119ef8a2583ec97e985e1e",
+    "url": "/lyon-0919-react-p2-springfield-vice/static/media/donut.f51f0b31.png"
+  },
+  {
+    "revision": "4f3e05baff80334500ed02a320bf4927",
+    "url": "/lyon-0919-react-p2-springfield-vice/static/media/poubelles.4f3e05ba.png"
+  },
+  {
+    "revision": "461f04be64627a380824fd32ec6eb200",
+    "url": "/lyon-0919-react-p2-springfield-vice/static/media/ripchain.461f04be.png"
+  }
+]);
