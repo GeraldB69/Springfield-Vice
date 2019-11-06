e63fb452bc375d22170903e4f35571025b1598bd
Anicia Tarlao <tarlaoanicia@gmail.com>
2019-11-06 10:48:39 +0100

    creation bart

---
+++ springfield_vice/src/components/Bart.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import config from "./configSpringfieldVice.json";
+
+class Bart extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			
+		};
+	}
+
+	render() {
+		
+		return (
+			<div>
+				<div style={homerZone}>
+					
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Bart;
