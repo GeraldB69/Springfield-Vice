a7eeacbb6033a46901aeee3d0432344eca3ffd6b
Cha-dieleman <chadieleman@gmail.com>
2019-11-07 10:41:09 +0100

    implémentation fonction affichage Jauge Santé

--- springfield_vice/src/components/Health.js
+++ springfield_vice/src/components/Health.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
-import duff from './img/duff.jpg';
+import biere from './img/biere.png';
+import biere_no from './img/biere_no.png';
+
 
 class Health extends Component {
     constructor(props){
@@ -8,13 +10,100 @@ class Health extends Component {
         };
     }
 
-    render(){
-        const {compteurBiere, compteurObst} = this.props;
-        console.log({compteurBiere})
-        let diff = this.props.compteurBiere - this.props.compteurObst;
-        return (
-        <p>{compteurBiere},{compteurObst}, result : {diff}</p>
+    counterHealth = () => {
+        let diff = 3 + this.props.beerCounter - this.props.obstCounter;
+
+        const styleDisplay = {  width : "30px",
+                                height : "auto",
+                                position: "absolute",
+                                left: "10px",
+                                top: "10px"};
+
+        const styleDisplayNone = {  
+                                display: "none",
+                                width : "30px",
+                                height : "auto",
+                                position: "absolute",
+                                left: "10px",
+                                top : "10px"};
+
+        const grappeBiereStyleDisplay = (<div>
+                                            <img src={biere} alt="duff beer" style={styleDisplay} />
+                                            <img src={biere_no} alt="empty beer" style={styleDisplayNone} />
+                                        </div>);
         
+        const grappeBiereStyleDisplayNone = (<div>
+                                            <img src={biere} alt="duff beer" style={styleDisplayNone} />
+                                            <img src={biere_no} alt="empty beer" style={styleDisplay} />
+                                        </div>);
+
+        switch(diff){
+            case 0 :
+                return (<div>   {grappeBiereStyleDisplayNone},
+                                {grappeBiereStyleDisplayNone},
+                                {grappeBiereStyleDisplayNone},
+                                {grappeBiereStyleDisplayNone},
+                                {grappeBiereStyleDisplayNone}
+                        </div>);
+            case 1:
+                return (<div>   {grappeBiereStyleDisplay},
+                                {grappeBiereStyleDisplayNone},
+                                {grappeBiereStyleDisplayNone},
+                                {grappeBiereStyleDisplayNone},
+                                {grappeBiereStyleDisplayNone}
+                        </div>);
+            case 2:
+                return (<div>   {grappeBiereStyleDisplay},
+                                {grappeBiereStyleDisplay},
+                                {grappeBiereStyleDisplayNone},
+                                {grappeBiereStyleDisplayNone},
+                                {grappeBiereStyleDisplayNone}
+                        </div>);
+            case 3:
+                return (<div>   {grappeBiereStyleDisplay},
+                                {grappeBiereStyleDisplay},
+                                {grappeBiereStyleDisplay},
+                                {grappeBiereStyleDisplayNone},
+                                {grappeBiereStyleDisplayNone}
+                        </div>);
+            case 4:
+                return (<div>   {grappeBiereStyleDisplay},
+                                {grappeBiereStyleDisplay},
+                                {grappeBiereStyleDisplay},
+                                {grappeBiereStyleDisplay},
+                                {grappeBiereStyleDisplayNone}
+                        </div>);
+            case 5:
+                return (<div>   {grappeBiereStyleDisplay},
+                                {grappeBiereStyleDisplay},
+                                {grappeBiereStyleDisplay},
+                                {grappeBiereStyleDisplay},
+                                {grappeBiereStyleDisplay}
+                        </div>);
+            default :
+                return (<div>   {grappeBiereStyleDisplay},
+                                {grappeBiereStyleDisplay},
+                                {grappeBiereStyleDisplay},
+                                {grappeBiereStyleDisplay},
+                                {grappeBiereStyleDisplay}
+                        </div>);
+
+        }
+
+
+    }
+
+
+    render(){
+        const styleDivParentBieres = {
+            width: "200px",
+            height: "auto",
+            borderRadius: "20px",
+            backgroundColor: "white",
+            opacity: ".7"
+        }
+        return (
+          <div id="divHealth" style={styleDivParentBieres}>{this.counterHealth()}</div>
         )
     }
 }
