ba44cdc337637be971734aa2e3ed67437fd1a17b
irvintarlao <irvin.tarlao@gmail.com>
2019-11-07 18:06:08 +0100

    homepage update

--- springfield_vice/src/pages/Start.js
+++ springfield_vice/src/pages/Start.js
@@ -3,6 +3,11 @@ import { Link } from 'react-router-dom'
 import Modal from '../components/Modal';
 import config from "../components/configSpringfieldVice.json";
 import "./start.css";
+import springfieldvice from "../components/img/springfieldvice.png"
+import bart from "../components/img/bart2blur.png"
+import homer from "../components/img/homerjumping.png"
+import triangle_icon from "../components/img/triangle_icon.png"
+
 
 class Start extends Component {
   constructor(props) {
@@ -24,22 +29,33 @@ class Start extends Component {
 
     return(
       <div id="init_div">
- 	     <div className="start_div" style={{display: this.state.firstScreen ? "block" : "none" }}>
-        <img className="left_img" src="logo512.png" alt="Springfield Vice" />
-        <img className="home_screen" src='bart-ass.png' alt='Bart Simpsons' />
-        <Link to={{ pathname: "/", search: "?modal=true" }}>
-        <button className="right_doh">
-          <img src='homer-doh.png' alt='Homer d`oh' />
-          <span className="text">Options</span>
-        </button>
-        </Link>
-        <Link to="/game">
-        <button id="start" className="right_button" onClick={() => this.startingGame()}>
-          <span className="text">Catch me...<br />if you can !</span>
-          <span className="logo">&#9654;</span>
-        </button>
-        </Link>
-      </div>
+          <div className="start_div" style={{display: this.state.firstScreen ? "block" : "none" }}>
+
+              <img className="sprvicelogo" src={springfieldvice} alt='logo springfield vice' />
+              <div className="homerjumping">
+                  <img className="homerjumpingimg" src={homer} alt='homerjumping' />
+              </div>
+              <div className="bart">
+                  <img className="bartimg" src={bart} alt='bart' />
+              </div>
+              <div className="homervsbart">
+                  <div className="neon">BART vs HOMER</div>
+              </div>
+              
+
+              <Link to={{ pathname: "/", search: "?modal=true" }}>
+              <div className="options">
+                <span>options</span>
+              </div>
+              </Link>
+
+              <Link to="/game">
+              <div id="start" className="start_button" onClick={() => this.startingGame()}>
+                <span>play</span>
+              </div>
+              </Link>
+
+          </div>
 
       {params.get("modal") && (
         <Modal
